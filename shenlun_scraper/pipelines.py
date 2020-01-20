12c4cb23f075f9cfeeb72133f95f21bd5bb40238
SoPudge <abcd666@gmail.com>
2020-01-20 23:03:32 +0800

    [ADD]init commit

---
+++ shenlun_scraper/pipelines.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+
+class ShenlunScraperPipeline(object):
+
+    def __init__(self):
+
+        self.workdir = os.getcwd()
+        logger.info(os.getcwd())
+        
+    def process_item(self, item, spider):
+        return item
+
+class PrepareResPipeline(object):
+    '''
+    Description:
+        根据item内容生成text.html,ncx,opf文件
+    '''
+
+    def process_item(self, item, spider):
+
+        title = item['title']
+        author = item['author']
+        content = ''.join("<p class=\"content\">"+i+"</p>" for i in item['content'])
+        pub_date = item['pub_date']
+
+        os.mkdir('../result/%s-%s-%s' % (pub_date, title, author))
+
+        #读取模板opf文件获取其中内容
+        with open('../resources/temp/title.opf','rt') as f:
+            opf = f.read()
+
+        #读取css文件
+        with open('../resources/temp/style.css','rt') as f:
+            stylecss = f.read()
+
+        #写入OPF文件
+        with open ('../result/%s-%s-%s/%s-%s-%s.opf' % (pub_date,title,author,pub_date,title,author),'at') as f:
+            opf = opf.replace('$title',title).replace('$author',author)
+            f.write(opf)
+
+        #写入css文件
+        with open ('../result/%s-%s-%s/style.css' % (pub_date,title, author),'at') as f:
+            f.write(stylecss)
+
+        #写入text.html文件
+        pagestart = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><title>%s</title><link type="text/css" href="style.css" rel="Stylesheet"/></head><body>' % title
+        pageend = '</body></html>'
+        pagetitle = '<h2>%s</h2>' % title
+        with open ('../result/%s-%s-%s/text.html' % (pub_date, title, author),'at') as f:
+            f.write(pagestart)
+            f.write(pagetitle)
+            f.write(content)
+            f.write(pageend)
+
+        logger.info('书籍资源制作完毕：%s' % pub_date+title)
+
+        return item
+
+class PackResToMobiPipeline(object):
+    '''
+    Description:
+        通过amazon kindlegen对mobi资源进行打包
+    '''
+
+    def __init__(self):
+        '''
+        Description:
+            列出所有需要的路径信息
+
+        '''
+
+    def process_item(self, item, spider):
+
+        title = item['title']
+        author = item['author']
+        pub_date = item['pub_date']
+
+        workdir = os.getcwd() + '/../result/%s-%s-%s' % (pub_date, title, author)
+
+        try:
+            opf_name = [i for i in os.listdir('../result/%s-%s-%s/' % (pub_date, title, author)) if 'opf' in i][0]
+            workopf = os.getcwd() + '/../result/%s-%s-%s/' % (pub_date, title, author) + opf_name
+        except:
+            logger.info('%s fault' % title)
+
+        workkindlegen = os.getcwd() + '/../resources/kindlegen'
+        #out = os.popen("%s -c1 -dont_append_source -locale zh \"%s\"" % (workkindlegen,workopf)).read()
+        out = os.popen("%s -c1 -dont_append_source -locale zh \"%s\"" % (workkindlegen,workopf)).read()
+        logger.info(out)
+        #os.system("%s -c1 -dont_append_source -locale zh \"%s\"" % (workkindlegen,workopf))
---
+++ shenlun_scraper/spiders/people_daily_opinion.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import re
+import logging
+from scrapy.exceptions import CloseSpider
+from scrapy import signals
+from pydispatch import dispatcher
+from  shenlun_scraper.items import ShenlunScraperItem
+
+logger = logging.getLogger(__name__)
+
+class PeopleDailyOpinionSpider(scrapy.Spider):
+    '''
+    Description:
+        抓取人民网当中人民时评所有内容，parse方法处理列表，yield到parse_info方法处理详细内容
+        交由pipelines方法写入到html，方便直接转换到mobi文件
+
+        通过正则表达式测试当前页是否有“下一页”字样存在，如果有则在url当中加1生成下一页url
+    '''
+
+    name = 'opinion'
+    allowed_domains = ['opinion.people.com.cn']
+    start_urls = ['http://opinion.people.com.cn/GB/8213/49160/49219/index1.html']
+
+    def __init__(self):
+
+        self.count = 1
+        
+    def parse(self, response):
+
+        opinion_base_url = "http://opinion.people.com.cn/"
+        index_base_url = "http://opinion.people.com.cn/GB/8213/49160/49219/index%s.html"
+
+        #opinion_title = response.xpath("//a[@class='abl']//text()").getall()
+        opinion_urls = response.xpath("//a[@class='abl']//@href").getall()
+        opinion_url = [opinion_base_url + i for i in opinion_urls]
+
+        for content_url in opinion_url:
+            yield scrapy.Request(url=content_url, callback=self.parse_info)
+
+        next_url = response.xpath("//td[@class='t10l14bl']").re('下一页')
+        if next_url:
+            self.count = self.count + 1
+            next_page = index_base_url % self.count
+            yield scrapy.Request(url=next_page, dont_filter=True, callback=self.parse)
+
+    
+    def parse_info(self, response):
+
+
+        item = ShenlunScraperItem()
+
+        #获取文章title,authr
+        content_title = response.xpath("//div[@class='clearfix w1000_320 text_title']/h1/text()").get()
+        try: 
+            title = re.match(r"人民日报人民时评(:|：)(.*)",content_title).group(2)
+        except:
+            title = content_title
+        author = response.xpath("//p[@class='author']/text()").get().replace('\xa0','')
+
+        #获取文章内容
+        origin_content = response.xpath("//div[@class='box_con']/p/text()").getall()
+        m = [i.strip('\n\t').strip('\u3000').strip('\xa0') for i in origin_content]
+        content = [i for i in m if i!='']
+
+        #获取发布时间
+        pub_date = ''.join(re.findall(r".*\/n1\/(\d{4})\/(\d{4,5})\/.*",response.url)[0])
+
+        item['title'] = title
+        item['author'] = author
+        item['content'] = content
+        item['pub_date'] = pub_date
+
+        yield item
+
+    #通过signal方式，定义爬虫在空闲的时候关闭
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs):
+        spider = super(PeopleDailyOpinionSpider, cls).from_crawler(crawler, *args, **kwargs)
+        crawler.signals.connect(spider.spider_idle, signal=signals.spider_idle)
+        return spider
+
+    def spider_idle(self, spider):
+        spider.logger.info('Spider closed: %s', spider.name)
