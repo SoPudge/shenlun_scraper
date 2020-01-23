50ed88e3a1a0e4d8cb485f9f93855b2cbc88b928
SoPudge <abcd666@gmail.com>
2020-01-23 14:38:07 +0800

    [MOD]运行脚本文件

--- run.sh
+++ run.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+#准备环境
+python3 -m pip install scrapy
+python3 -m pip install pydispatch
+
+#运行程序
+cd shenlun_scraper
+scrapy crawl opinion
+
+#打包结果
+zip -r -0 ../result.zip ../result
